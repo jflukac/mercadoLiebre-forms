d3fed1aa8a55da6a6bcdb0752e55a9657fca427b
jflukac <juanfranciscolukac@gmail.com>
2020-12-22 10:48:15 -0300

    correct create, edit and delete products

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,34 +1,51 @@
+const { name } = require('ejs');
 const fs = require('fs');
 const path = require('path');
+const setID = require ('../helpers/setID')
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const generateID = function() {
+	let ids = products.map((product) => product.id)
+	return setID(ids)
+}
+
 const newProduct = function(req) {
-	console.log(products[-1])
 	let newProduct = {
-		id: (products.length + 1),
-		name: req.body.name,
-		price: req.body.price,
-		discount: req.body.discount,
-		category: req.body.category,
-		description: req.body.description,
+		id: generateID(),
+		...req.body,
 		image: req.files[0].filename,
 	};
 	products.push(newProduct);
 	let productsJSON = JSON.stringify(products);
-	return productsJSON
+	fs.writeFileSync(productsFilePath, productsJSON)
+	return newProduct.id
 }
+
+const editProduct = function (req) {
+	products.forEach((product) => {
+		if (product.id == req.params.id) {
+			// product = { ...product, ...req.body, image: req.files[0].filename }
+			product.name = name
+			product.price = price
+			product.discount = Number(product.discount)
+			product.image = req.files[0].filename
+			product.category = category
+			product.description = description
+		}
+	})
+	const productJson = JSON.stringify(products)
+	fs.writeFileSync(productsFilePath, productJson)
+	return product.id
+}
+
 const deleteProduct = function(req) {
-	const product = products.find(item =>  item.id == req.params.id);
-	let index = products.indexOf(product);
-	let productsModify = products.splice(index, 1);
-	let productsJSON = JSON.stringify(productsModify);
-	return productsJSON
+	const productsNotDeleted = products.filter((product) => product.id != req.params.id)
+	let productsJSON = JSON.stringify(productsNotDeleted);
+	fs.writeFileSync(productsFilePath, productsJSON)
 }
-
-
 
 const controller = {
 	// Root - Show all products
@@ -44,7 +61,6 @@ const controller = {
 		} else {
 			product.price = toThousand (product.price)
 		}
-
 		res.render('detail', {product, title: product.name})
 	},
 
@@ -55,8 +71,8 @@ const controller = {
 	
 	// Create -  Method to store
 	store: (req, res, next) => {
-		fs.writeFileSync(path.join(__dirname, '../data/productsDataBase.json'), newProduct(req));
-		res.redirect('/products')
+		const id = newProduct(req);
+		res.redirect('/products/detail/' + id)
 	},
 
 	// Update - Form to edit
@@ -66,15 +82,14 @@ const controller = {
 	},
 	// Update - Method to update
 	update: (req, res, next) => {
-		// fs.writeFileSync(path.join(__dirname, '../data/productsDataBase.json'), deleteProduct(req));
-		// fs.writeFileSync(path.join(__dirname, '../data/productsDataBase.json'), newProduct(req));
-		res.render('detail', {product, title: product.name})
+		const id = editProduct(req)
+		res.redirect('/products/detail/' + id)
 	},
 
 	// Delete - Delete one product from DB
 	destroy : (req, res, next) => {
-		fs.writeFileSync(path.join(__dirname, '../data/productsDataBase.json'), deleteProduct(req));
-		res.redirect('/products')
+		deleteProduct(req)
+		res.redirect('/')
 	}
 };
 
